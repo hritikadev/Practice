9c45c75b1b5cad787c66f501efdc42f017d71238
Hritika Dev <hritikadev@gmail.com>
2023-09-08 22:57:41 +0530

    Time: 76 ms (31.67%) | Memory: 16.4 MB (90.82%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        # Create a dictionary to store character counts
+        dictionary = {}
+
+        # Iterate through the magazine and count characters
+        for char in magazine:
+            if char not in dictionary:
+                dictionary[char] = 1
+            else:
+                dictionary[char] += 1
+
+        # Iterate through the ransom note and check character counts
+        for char in ransomNote:
+            if char in dictionary and dictionary[char] > 0:
+                dictionary[char] -= 1
+            else:
+                return False
+        
+        return True
+        
